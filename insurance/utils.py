ccb8c57162c101c25285d2887c2b9cc7edc3c741
Rahulpatel78 <rahulpateltime143@gmail.com>
2023-01-23 13:44:28 +0530

    utils and config file updated

--- insurance/utils.py
+++ insurance/utils.py
@@ -7,7 +7,7 @@ from insurance.logger import logging
 import pandas as pd
 
 
-def get_collection_as_dataframe(database_name:str, collection_name:str):
+def get_collection_as_dataframe(database_name:str, collection_name:str)->pd.DataFrame:
     try:
         logging.info(f"Reading data frame database:{database_name} and collection: {collection_name}")
         df = pd.DataFrame(mongo_client[database_name][collection_name].find())
